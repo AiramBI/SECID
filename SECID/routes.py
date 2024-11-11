664378a9110d9ff02939def602c5114ee08758e2
AiramBI <airam.eng@outlook.com>
2024-11-11 10:19:29 -0300

    Update routes.py

--- SECID/routes.py
+++ SECID/routes.py
@@ -9,8 +9,7 @@ import os
 import logging
 from SECID.uploadarquivos import executarautomacao
 import uuid
-from flask_dropzone import Dropzone
-from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+
 
 logging.basicConfig(level=logging.INFO)
 
