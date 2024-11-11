1714f48929da1390234038ce1ccf9d1daf9a0b72
AiramBI <airam.eng@outlook.com>
2024-11-11 00:43:15 -0300

    Update __init__.py

--- SECID/__init__.py
+++ SECID/__init__.py
@@ -4,11 +4,16 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import sqlalchemy
 import os
+flask_dropzone
 # from flask_migrate import Migrate
+from flask_dropzone import Dropzone
 
 app = Flask(__name__)
+dropzone = Dropzone(app)
 
 # Configurações
+app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
 app.config['SECRET_KEY'] = '70898ff6cf8c6fc9a940820e7c211072'
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024  # Limita o tamanho do upload para 20MB, por exemplo
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Cache control for file uploads
