4c3912304f046d74060de687d4b68bd202f22559
AiramBI <airam.eng@outlook.com>
2024-11-11 11:07:13 -0300

    Update celery_worker.py

--- SECID/celery_worker.py
+++ SECID/celery_worker.py
@@ -1,15 +1,23 @@
 from celery import Celery
-from SECID import create_app, db  # Importe seu app Flask e o banco de dados
-import os
+from SECID import app, database  # Importa o app Flask e o banco de dados
 
 def make_celery(app):
+    # Configura o Celery com o broker e o backend definidos no Redis (ou conforme sua escolha)
     celery = Celery(
         app.import_name,
-        backend=os.getenv("REDIS_URL", "redis://localhost:6379/0"),
-        broker=os.getenv("REDIS_URL", "redis://localhost:6379/0")
+        backend=app.config.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0'),
+        broker=app.config.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
     )
     celery.conf.update(app.config)
+
+    # Defina o contexto da aplicação para tarefas do Celery
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():  # Garante que as tarefas sejam executadas no contexto do Flask
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
     return celery
 
-app = create_app()  # Substitua por como você cria sua aplicação Flask
+# Inicializa o Celery com o contexto do Flask
 celery = make_celery(app)
