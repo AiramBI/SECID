de9be9c4fcbe6213895aaa70342af864280f9875
AiramBI <airam.eng@outlook.com>
2024-12-03 00:30:35 -0300

    Update enviar_medicao.py

--- SECID/enviar_medicao.py
+++ SECID/enviar_medicao.py
@@ -13,147 +13,32 @@ from num2words import num2words
 
 
 
-def registrar_medicao():
+def registrar_medicao(coordenacao,observacoes_processo,caminho_arquivo,data_inicial, data_final, valor_total_prervisto, valor_atual_previsto, valor_total_inicial, valor_atual_inicial, valor_atual_medido, aditivo,inicial, numero_contrato, numero_medicao, cronograma_atualizado, reajustamento, reajustamento_total,valor_medicao
+medicoes, rerratificacao,processo_mae, objeto, documento_gestor_contrato, publicacao_comissao_fiscalização, lei_contrato,contrato,seguro_garantia,carta_solicitacao_prorrogacao_contratual,processo_rerratificacao,termo_aditivo,contratada,obra, fiscal_tecnico_1,id_fiscal_tecnico_1,fiscal_tecnico_2,id_fiscal_tecnico_2, gestor1,
+id_gestor,cnpj_empresa):
     login = os.getenv("LOGIN", "asantos2") #PRECISO MEXER NISSO ANTES DE IMPLEMENTAR VALORES FIXOS
     senha = os.getenv("SENHA", "Ivinhema1994#*#*#*") #PRECISO MEXER NISSO ANTES DE IMPLEMENTAR VALORES FIXOS
-    secretaria = os.getenv("SECRETARIA","SECID")  #PRECISO MEXER NISSO ANTES DE IMPLEMENTAR VALORES FIXOS
+    secretaria = os.getenv("SECRETARIA","SEFAZ")  #PRECISO MEXER NISSO ANTES DE IMPLEMENTAR VALORES FIXOS
     coordenacao = "SEFAZ/COOCPP" 
     observacoes_processo = os.getenv("OBSERVACOES_PROCESSO","") #ATÉ O MOMENTO NÃO TEM ATRIBUIÇÃO
     nomes_documentos = [
-        "01.Carta assinada pela empresa",
-        "02.Publicação da Comissão de Fiscalização",
-        "03.Planilha de Medição (PDF)",
-        "03.1.Planilha de Medição - Arquivo em Excel",
-        "04.Memória de Cálculo",
-        "05.Cronograma Físico - Financeiro",
-        "06.Diário de Obras",
-        "07.Relatório Fotográfico",
-        "08.Relação de Funcionários",
-        "09.Folha de ponto dos funcionários",
-        "10.GFD FGTS DIGITAL",
-        "10.1.DCTF WEB",
-        "11.Guias e Comprovantes de Pagamentos de FGTS",
-        "12.Folha de Pagamento",
-        "13.Comprovante de Pagamento de salários",
-        "14.Plano de segurança do Trabalho",
-        "15.Certidões atualizadas",
-        "15.1.Certidão de regularidade junto ao FGTS",
-        "15.2.Certidão negativa de débito trabalhista",
-        "15.3.Certidão negativa de débitos federais",
-        "15.4.Certidão de regularidade fiscal junto ao ICMS",
-        "15.5.Certidão de regularidade fiscal junto ao ISS",
-        "16.Contrato",
-        "17.ART emitida pelo CREA",
-        "18.Nota de empenho",
-        "19.Nota fiscal e ISS"
-    ]
-    caminho_arquivo = [
-    "medicao.documento_1",
-    "medicao.documento_2",
-    "medicao.documento_3",
-    "medicao.documento_3_1",
-    "medicao.documento_4",
-    "medicao.documento_5",
-    "medicao.documento_6",
-    "medicao.documento_7",
-    "medicao.documento_8",
-    "medicao.documento_9",
-    "medicao.documento_10",
-    "medicao.documento_10_1",
-    "medicao.documento_11",
-    "medicao.documento_12",
-    "medicao.documento_13",
-    "medicao.documento_14",
-    "medicao.documento_15",
-    "medicao.documento_15_1",
-    "medicao.documento_15_2",
-    "medicao.documento_15_3",
-    "medicao.documento_15_4",
-    "medicao.documento_15_5",
-    "medicao.documento_16",
-    "medicao.documento_17",
-    "medicao.documento_18",
-    "medicao.documento_19"]
-    # [medicao.documento_1 
-    #     medicao.documento_2,
-    #     medicao.documento_3,
-    #     medicao.documento_3_1,
-    #     medicao.documento_4,
-    #     medicao.documento_5,
-    #     medicao.documento_6,
-    #     medicao.documento_7,
-    #     medicao.documento_8,
-    #     medicao.documento_9,
-    #     medicao.documento_10,
-    #     medicao.documento_10_1,
-    #     medicao.documento_11,
-    #     medicao.documento_12,
-    #     medicao.documento_13,
-    #     medicao.documento_14,
-    #     medicao.documento_15,
-    #     medicao.documento_15_1,
-    #     medicao.documento_15_2,
-    #     medicao.documento_15_3,
-    #     medicao.documento_15_4,
-    #     medicao.documento_15_5,
-    #     medicao.documento_16,
-    #     medicao.documento_17,
-    #     medicao.documento_18,
-    #     medicao.documento_19]
-    data_inicial = "01/08/2024"
-    data_final = "15/08/2024"
-    valor_total_previsto = 5000000.00
-    valor_atual_previsto = 1000000.00
-    valor_total_inicial = 4000000.00
-    valor_atual_inicial = 3000000.00
-    valor_atual_medido = 2000000.00
-    aditivo = 150
-    inicial = 300
-    #Documentos Necessários Nota técnica
-    numero_contrato = "021/2024"
+        "01.Carta assinada pela empresa","02.Publicação da Comissão de Fiscalização","03.Planilha de Medição (PDF)","03.1.Planilha de Medição - Arquivo em Excel",
+        "04.Memória de Cálculo","05.Cronograma Físico - Financeiro","06.Diário de Obras","07.Relatório Fotográfico",
+        "08.Relação de Funcionários","09.Folha de ponto dos funcionários","10.GFD FGTS DIGITAL","10.1.DCTF WEB","11.Guias e Comprovantes de Pagamentos de FGTS",
+        "12.Folha de Pagamento","13.Comprovante de Pagamento de salários","14.Plano de segurança do Trabalho","15.Certidões atualizadas","15.1.Certidão de regularidade junto ao FGTS","15.2.Certidão negativa de débito trabalhista",
+        "15.3.Certidão negativa de débitos federais","15.4.Certidão de regularidade fiscal junto ao ICMS","15.5.Certidão de regularidade fiscal junto ao ISS","16.Contrato",
+        "17.ART emitida pelo CREA","18.Nota de empenho","19.Nota fiscal e ISS"]
     periodo_medicao = f"'{data_inicial}' a '{data_final}'"
-    numero_medicao = "15"
-    cronograma_atualiado = "74850757"
     porcentagem_concluida_previsto = valor_atual_previsto/valor_total_previsto
-    valor_atual_previsto = valor_atual_previsto
     porcentagem_concluida_inicial = valor_atual_inicial/valor_total_inicial
-    valor_atual_inicial = valor_atual_inicial
     porcentagem_concluida_medida = valor_atual_medido/valor_total_previsto
-    valor_atual_medido = valor_atual_medido
-    reajustamento = 0
-    reajustemento_total = 1000000
-    valor_medicao = 504152.00
-    medicoes = ["colocar as medicoes das medicoes"]
-    rerratificacao = 1000000.00
     prazo_aditivo = f"'{aditivo}' ('{num2words(aditivo, lang ='pt_BR')}"
-    prazo_inicial = f"'{inicial}' ('{num2words(inicial, lang ='pt_BR')}"
-    processo_mae = "SEI-040009/000654/2024"#NUMERO DO PROCESSO MÃE (BUSCAR NUMERO DO PROCESSO MÃE) #SINALIZAR NO CADASTRO O SEI DO PROCESSO MÃE
-    objeto = "objeto"
-    documento_gestor_contrato = "74850775"
-    publicacao_comissao_fiscalização = "021/2024 de 15 de Novembro de 2024"
-    lei_contrato = "8.666, de 21 de junho de 1993"
-    
-    
-    #Documento Necessário CHECKLIST
-    contrato = "87715423" #CONTRATO (BUSCAR CONTRATO NO PROCESSO MÃE) # SINALIZAR NO CADASTRO O CÓDIGO DO DOCUMENTO CONTRATO NO PROCESSO MÃE
-    seguro_garantia = "87715423" #SEGURO GARANTIA(BUSCAR SEGURO GARANTIA NO PROCESSO MÃE) #SINALIZAR NO CADASTRO O CÓDIGO DO DOCUMENTO SEGURO GARANTIA PROCESSO MATRIZ
-    carta_solicitacao_prorrogacao_contratual = "87715423" #CARTA DE SOLICITAÇÃO DE PRORROGAÇÃO CONTRATUAL (BUSCAR CARTA NO PROCESSO INICIAL ) SINALIZAR NO CADASTRO DO DOCUMENTO CARTA DE SOLICITACAO PROGRRAGACAO 
-    processo_rerratificacao = "SEI-260006/010954/2024" #PROCESSO DE RERRATIFICAÇÃO ( BUSCAR INDEX NO PROCESSO ORIGINAL)# SINALIZAR NO CADASTRO DO PROCESSO INICIAL
-    termo_aditivo = "87715447" #TERMO ADITIVO ( BUSCAR TERMO ADITIVIO NO PROCESSO MÃE)# SINALIZAR NO CADASTRO DO PROCESSO MÃE
-    contratada = "empresa1234"
-    obra = "Cachoeiras de Macacu I - Praça Village"
-    
-    especificacao_processo = f"'{numero_medicao}'ª Medição - Obra:'{obra}' - Período:'{periodo_medicao}'" #Numero da Medicação -  OOBRA -  COMPETENCIA
-    fiscal_tecnico_1 = "Fulano da Silva Santos"
-    id_fiscal_tecnico_1 = "5115709-8"
-    fiscal_tecnico_2 = "Beltrano da Silva Santos"
-    id_fiscal_tecnico_2 = "5115709-9"
-    gestor1 = "Ciclano da Silva dos Santos"
-    id_gestor = "5115709-0"
+    prazo_inicial = f"'{inicial}' ('{num2words(inicial, lang ='pt_BR')}" 
+      
+    especificacao_processo = f"'{numero_medicao}'ª Medição - Obra:'{obra}' - Período:'{periodo_medicao}'"
     carimbo_fiscal1 = f"{fiscal_tecnico_1}\nFiscal Técnico\n{id_fiscal_tecnico_1}"
     carimbo_fiscal2 = f"{fiscal_tecnico_2}\nFiscal Técnico\n{id_fiscal_tecnico_2}"
     carimbo_gestor = f"{gestor1}\nGestor do Contrato\n{id_gestor}"
-    cnpj_empresa = "01.158.700/0001-20"
     
     def  indexar_checklist_1():
         navegador.switch_to.default_content()
@@ -492,7 +377,7 @@ def registrar_medicao():
     
         #Movimenta o cursor até o início do texto localizado Referencia numero da medição
         corpoTexto.send_keys(Keys.ARROW_RIGHT * 760)
-        corpoTexto.send_keys(Keys.ENTER)
+        corpoTexto.send_keys(Keys.ENTER)# alterar essa parte
         medicoes= [
         "1ª Medição período 01/09/2022 a 30/09/2022 no valor de R$ 668.194,70;",
         "2ª Medição período 01/10/2022 a 31/10/2022 no valor de R$ 0,00;",
