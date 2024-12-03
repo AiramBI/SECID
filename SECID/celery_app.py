9333e52119754cbca7f5901b4db30b45406f9a0f
AiramBI <airam.eng@outlook.com>
2024-12-02 21:17:02 -0300

    Delete SECID/celery_app.py

--- SECID/celery_app.py
+++
@@ -1,5 +0,0 @@
-from SECID import create_app  # Função de fábrica que cria o app
-from SECID.make_celery import make_celery  # Função make_celery para configurar o Celery
-
-app = create_app()  # Cria uma nova instância do app Flask
-celery = make_celery(app)  # Configura o Celery com o contexto do app
