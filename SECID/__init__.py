1148b4076e127f0a346d77585ac5dacd5caa5fed
AiramBI <airam.eng@outlook.com>
2024-12-06 08:48:35 -0300

    Update __init__.py

--- SECID/__init__.py
+++ SECID/__init__.py
@@ -15,7 +15,7 @@ app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.pdf'  # Permitir apenas arquivos PD
 app.config['DROPZONE_MAX_FILE_SIZE'] = 10  # Tamanho máximo de cada arquivo em MB
 app.config['DROPZONE_TIMEOUT'] = 300000  # 5 minutos em milissegundos
 app.config['SECRET_KEY'] = '70898ff6cf8c6fc9a940820e7c211072'
-app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024  # Limita o tamanho do upload para 20MB, por exemplo
+#app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024  # Limita o tamanho do upload para 20MB, por exemplo
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Cache control for file uploads
 
 # Configuração do diretório de uploads
