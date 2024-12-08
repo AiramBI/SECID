3e81506611a3ad19019007b1b99f57649fa4cdbf
AiramBI <airam.eng@outlook.com>
2024-12-08 20:46:51 -0300

    Update __init__.py

--- SECID/__init__.py
+++ SECID/__init__.py
@@ -5,8 +5,26 @@ from flask_login import LoginManager
 import sqlalchemy
 import os
 from flask_dropzone import Dropzone
-    
+from celery import Celery
+
 app = Flask(__name__)
+
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+    return celery
+
+# Configuração de Redis como backend e broker
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+celery = make_celery(app)
+
+
 
 # Configurações
 app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
